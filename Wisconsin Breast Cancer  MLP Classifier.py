8dbe46dd8f76e525136dfb128ba63f071afdd663
mohammadAsadolahi <79268727+mohammadAsadolahi@users.noreply.github.com>
2021-07-24 19:31:09 +0430

    Update Wisconsin Breast Cancer  MLP Classifier.py

--- Wisconsin Breast Cancer  MLP Classifier.py
+++ Wisconsin Breast Cancer  MLP Classifier.py
@@ -1,6 +1,6 @@
 #Wisconsin Breast Cancer  MLP Classifier
 #WRITTEN BY MOHAMMAD ASADOLAHI
-#mohmad_asadolahi@yahoo.com
+#Mohamad.asa1994@gmail.com
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
